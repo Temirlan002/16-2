7f5fc991d00c5b0a914eca5bece7d49b72496e99
Temirlan006 <ahunzanovtemirlan@gmail.com>
2022-07-06 21:20:39 +0600

    lesson1

---
+++ lesson1/main.js
@@ -0,0 +1,63 @@
+const a = "a"; // string
+
+
+let b = 45; // number
+
+const bool = true; //boolean
+const bool2 = false;
+
+const obj = { // object
+    key: 'value',
+    username: 'Sara',
+    data: {
+        first_name: "jack",
+        last_name: "Barbaro"
+    }
+}
+
+// console.log(obj.data.last_name)
+
+const arr = ["Aydin", true, false, 35] //array
+// console.log(arr[1])
+
+const arr2 = [
+    {
+        key: 'value'
+    },
+    {
+        key: 'value2'
+    },
+    {
+        key: 'value3'
+    }
+]
+console.log(arr2[2].key)
+
+// console.log(typeof obj)
+
+// const name = prompt("Как вас зовут?")
+// console.log(name)
+
+// const age = alert("вам еще нет 18!")
+
+// const first_number = prompt("Первое число?")
+// const last_number = prompt("Второе число?")
+//
+// if(first_number > last_number){
+//     console.log(first_number + ">" + last_number)
+// } else if(last_number > first_number){
+//     console.log(first_number + "<" + last_number)
+// } else {
+//     console.log(first_number + "=" + last_number)
+// }
+
+
+// const age = prompt("Сколько вам лет?")
+//
+// if(age < 18){
+//     console.log("вам нельзя входить")
+// } else if(age > 18){
+//     console.log("вам можно")
+// } else {
+//     console.log("поживи еще годик")
+// }
